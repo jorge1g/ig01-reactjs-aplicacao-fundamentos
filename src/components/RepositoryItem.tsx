012af7580095b695cacaa82c325837a16a6623ae
Jorge Gonzaga <jsgonzaga@uol.com.br>
2021-03-13 17:06:30 -0300

    Chapter 1 concluido

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -12,7 +12,7 @@ export function RepositoryItem(props: RepositoryItemProps) {
             <strong>{props.repository.name}</strong>
             <p>{props.repository.description}</p>
             <a href={props.repository.html_url}>
-                Acessar repositório
+                Acessar repositório GitHub
             </a>
         </li>
     );
